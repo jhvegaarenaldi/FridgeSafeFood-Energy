1d36e9afe141fdb7784a5b3c3eaa6d70594f2561
jhvegaarenaldi <jh.vega.arenaldi@gmail.com>
2016-10-05 23:06:37 -0300

    Add files via upload

--- FrigdeSafeFood-Energy.ino
+++ FrigdeSafeFood-Energy.ino
@@ -11,12 +11,12 @@
 char ServerArtik[] = "api.artik.cloud";  
 int portTCP = 443; //(port 443 is default for HTTPS)
 WiFiSSLClient clientArtik;
-String AuthorizationData = "Authorization: Bearer c539ecdcc6514c6ba351a27d16d70fa6";
+String AuthorizationData = "Authorization: Bearer <INSERT YOU ID>";
 char buffer[200];
 
 // Parameters WIFI conection
-char ssid[] = "FAMILIA";      //  your wifi network SSID (name)
-char pass[] = ".Bbeach.123...";  // your password wifi
+char ssid[] = "";      //  your wifi network SSID (name)
+char pass[] = "";  // your password wifi
 int status = WL_IDLE_STATUS;
 
 // Variable and Constant used in Program 
@@ -27,7 +27,7 @@ unsigned long currentMillis = 0, previousMillis=0;  // variable used to control 
 unsigned signalDuration=300; // lapsed time long BEEP sound
 unsigned int CountSendAlert=1; // variable used to control message to Artik
 const long intervalBEEP = 20*1000; // => 20 secons Door Open -> Sound Alarm
-const int IntervalSendAlert = 1 ; // => variable used to control the Alert interval -> in MINUTES ( It must be set equal to RULE ARTIK CLOUD).
+const int IntervalSendAlert = 5 ; // => variable used to control the elapsed time  -> in MINUTES ( It must be set equal to RULE ARTIK CLOUD).
 const long intervalMAIL = IntervalSendAlert * 60 * 1000; // => the value (IntervalSendAlert) means minutes Door Open too long -> Message to Artik
 
 void setup ()
@@ -106,7 +106,7 @@ int loadBuffer(int Value ) {
    StaticJsonBuffer<200> jsonBuffer; // reserve spot in memory
 
    JsonObject& root = jsonBuffer.createObject(); // create root objects
-     root["sdid"] = "7912145b617243a7a21412f9644678c4"; // FIX 
+     root["sdid"] = "<YOU SDID>"; // FIX 
      root["type"] = "message";
 
    JsonObject& dataPair = root.createNestedObject("data"); // create nested objects
